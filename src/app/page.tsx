520878b24508b4125f73f63ae131e56eca265f5a
Asadkhan12334455 <asada1l2i3z4a5@gmail.com>
2024-10-14 06:04:02 +0500

    First

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import Hero from './components/Hero'
 export default function Home() {
   return (
     <>
-      <Header />
-      <Hero />
-      <Footer />
+      <Header/>
+      <Hero/>
+      <Footer/>
     </>
   )
 }
